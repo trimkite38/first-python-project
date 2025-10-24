644b5206ab5723cb9bb25b127730c8cd3d092f41
private figures <ibrahimdiakite38@icloud.com>
2025-10-24 04:56:07 +0100

    Day 1: Experimented with variables and described where i live and what i enjoy

---
+++ day1_basics.py
@@ -0,0 +1,24 @@
+name = "TrimKite38"
+age = 22
+height = 5.11
+favourite_sport = "Football"
+is_active = "True"
+favourite_show = "Scandal"
+favourite_genre_of_music = "HipHop"
+
+print("Hi my name is", name)
+print("i am", age, "years old.")
+print("My favourite sport is", favourite_sport)
+print("Am i currently active in sports?", is_active)
+print("my favourite show is", favourite_show , "i also watch anime")
+print("My favourite genre of music is", favourite_genre_of_music)
+
+age = 23
+favourite_sport = "Basketball"
+enjoy = "Code"
+city = "London"
+
+print("\nAfter updating variables:")
+print("Now i am", age, "years old and i play", favourite_sport)
+print("In my free time i like to", enjoy)
+print("I live in the gloomy weather of", city)
